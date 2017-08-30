8c7a3525a4e117521e513ccb9686dbc9a2e310c2
NvkAnirudh <nuti.krish4@gmail.com>
2017-08-30 22:51:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one', function(req, res){
+app.get('/article-one', function(req, res){
     res.send('article one is defined here');
 });
 
 
-app.get('article-two', function(req, res){
+app.get('/article-two', function(req, res){
     res.send('article two is defined here');
 });
 
 
-app.get('article-three', function(req, res){
+app.get('/article-three', function(req, res){
     res.send('article three is defined here');
 });
 
