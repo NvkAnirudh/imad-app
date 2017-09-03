b35c10e02d65421e3933f95c0fb20b9e41399735
NvkAnirudh <nuti.krish4@gmail.com>
2017-09-04 00:55:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,8 @@ element.innerHTML='Click me';
 var img=document.getElementById('madi');
 var marginLeft= 0;
 function marginRight(){
-    marginLeft= marginLeft - 10;
-    img.style.marginLeft= marginLeft + 'px';
+    marginLeft= marginLeft + 10;
+    img.style.marginTop= marginLeft + 'px';
 
 }
 
