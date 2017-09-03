f0203787615bb6e4d1e194862bd232443f6d1cb7
NvkAnirudh <nuti.krish4@gmail.com>
2017-09-04 00:37:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element=document.getElementById('mmain-text');
 element.innerHTML='Changed content';
+
+var img=document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginleft= '100px';
+};
