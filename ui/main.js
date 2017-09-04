7b3737947e9f8ea34f1a4db56bd4eaf4c13ba911
NvkAnirudh <nuti.krish4@gmail.com>
2017-09-05 01:27:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,31 @@
-var button = document.getElementById('counter');
-button.onclick= function() {
+
+var submitName = document.getElementById('submit_btn');
+submitName.onclick= function() {
+    
     
     var request = new XMLHttpRequest();
     
     request.onreadystatechange= function() {
         if(request.readyState === XMLHttpRequest.DONE) {
             if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                var names = request.responseText;
+                names=JSON.parse(names);
+                list ='';
+                for(i=0;i<names. length;i++){
+                    list += '<li>' + names[i] + '</li>';
+                    }
+                var ul= document.getElementById('ul_id');
+                ul.innerHTML= list;
             }
         }
     };
-    request.open('GET', 'http://nutikrish4.imad.hasura-app.io/counter', true);
+    
+    var nameInput = document.getElementById();
+    var name= nameInput.value;
+    request.open('GET', 'http://nutikrish4.imad.hasura-app.io/', true);
     request.send(null);
      
 };
 
-var submitName = document.getElementById('submit_btn');
-submitName.onclick= function() {
-    
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    list ='';
-    for(i=0;i<names. length;i++){
-        list += '<li>' + names[i] + '</li>';
-        }
-    var ul= document.getElementById('ul_id');
-    ul.innerHTML= list;
-};
+
+   
